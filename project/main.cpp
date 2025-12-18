d2b454fdfc7231fb591ea9269bcc7e27bd342a14
Ehsan <e.motevasel@gmail.com>
2025-12-18 13:03:50 +0330

    added simple calculator functions like SUM SUB and MUL of n numbers

--- project/main.cpp
+++ project/main.cpp
@@ -1,1 +1,28 @@
 //please write your functions here
+#include <iostream>
+using namespace std;
+
+
+double Sum(double arr[],int size)
+{double SUMM=0;
+    for(int i=0;i<size;i++)
+    {SUMM+=arr[i];}
+
+    return SUMM;
+}
+double Sub(double arr[],int size)
+{double SUBB=arr[0];
+    for(int i=1;i<size;i++)
+    {SUBB-=arr[i];}
+
+    return SUBB;
+    
+}
+double Mul(double arr[],int size)
+{double MULL=1;
+    for(int i=0;i<size;i++)
+    {MULL*=arr[i];}
+
+
+    return MULL;
+}
