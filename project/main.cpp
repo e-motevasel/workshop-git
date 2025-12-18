99f95ea8c54f4b2d3bdbb44326a2a2ec704c3af8
Ehsan <e.motevasel@gmail.com>
2025-12-18 23:45:54 +0330

    added int main

--- project/main.cpp
+++ project/main.cpp
@@ -75,3 +75,83 @@ void Solver(double a,double b,double c)
         }
      }
 }
+int main()
+{
+    cout<<"Please select your function: "<<endl;
+    cout<<"1.Simple Cal"<<endl<<"2.Solve quadratic EQ"<<endl<<"3.Advanced Cal"<<endl<<"4.Exit"<<endl;
+    int n;
+    do{
+        int c;
+        cin>>n;
+        if(n==1)
+        {double arr[10000];
+         cout<<"1.SUM"<<endl<<"2.SUB"<<endl<<"3.MUL"<<endl;
+            cin>>c;
+            if(c==1)
+            {cout<<"length of you array: ";int x;
+             cin>>x;
+             for(int i=0;i<x;i++)
+             {cin>>arr[i];}
+             cout<<Sum(arr,x)<<endl;
+             break;
+            }
+            if(c==2)
+            {int x;cout<<"length of you array: ";
+             cin>>x;
+             for(int i=0;i<x;i++)
+             {cin>>arr[i];}
+             cout<<Sub(arr,x)<<endl;
+             break;
+            }
+            if(c==3)
+            {int x;cout<<"length of you array: ";
+             cin>>x;
+             for(int i=0;i<x;i++)
+             {cin>>arr[i];}
+             cout<<Mul(arr,x)<<endl;
+             break;
+            }        
+        }
+        if(n==2)
+        {double a,b,d;
+	 cin>>c;
+	 cout<<"1.Daraje 1"<<endl<<"2.Daraje 2"<<endl;
+		if(c==1)
+		{cin>>b>>d;Solver(0,b,d);break;}
+		if(c==2)
+		{cin>>a>>b>>d;Solver(a,b,d);break;}
+     
+        }
+        if(n==3)
+        {cout<<"1.GCD"<<endl<<"2.LCD"<<endl<<"3.C(n,k)"<<endl;
+            cin>>c;
+            if(c==1)
+            {int a,b;cout<<"your numbers: ";
+                cin>>a>>b;
+                cout<<gcd(a,b)<<endl;
+                break;
+            }
+            if(c==2)
+            {
+                int a,b;cout<<"your numbers: ";
+                cin>>a>>b;
+                cout<<lcm(a,b)<<endl;
+                break;
+            }
+            if(c==3)
+            {
+                int a,b;cout<<"your numbers: ";
+                cin>>a>>b;
+                cout<<C(a,b)<<endl;
+                break;
+            }
+        }
+        if(n==4)
+        {break;}
+        else
+        {cout<<"Invalid number please try again: "<<endl;
+                cout<<"1.Simple Cal"<<endl<<"2.Solve quadratic EQ"<<endl<<"3.Advanced Cal"<<"4.Exit"<<endl;}
+
+
+    }while(n!=4);
+}
